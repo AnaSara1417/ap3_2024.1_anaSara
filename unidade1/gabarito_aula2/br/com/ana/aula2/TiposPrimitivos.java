28b4cbff9fed79e9e940d0789e51f7ac4d25d25b
bezerrapedro <obezerra.pedro@gmail.com>
2024-07-19 20:57:25 -0300

    Update TiposPrimitivos.java

--- unidade1/gabarito_aula2/br/com/ana/aula2/TiposPrimitivos.java
+++ unidade1/gabarito_aula2/br/com/ana/aula2/TiposPrimitivos.java
@@ -4,6 +4,7 @@ package br.com.ana.aula2;
 public class TiposPrimitivos {
     public static void main(String[]args){
 
+        //Tipos Primitivos: Crie um programa que declare variáveis de todos os tipos primitivos em Java (byte, short, int, long, float, double, boolean, char) e uma variável do tipo String. Atribua valores a elas e imprima esses valores no console.
 
     }
 
