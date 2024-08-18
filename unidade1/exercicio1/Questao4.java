702a8061f968fdd60d7462b4b4e97f62e2aab166
Ana Sara <58222006+AnaSara1417@users.noreply.github.com>
2024-08-18 20:38:49 -0300

    Resolução Questão4

---
+++ unidade1/exercicio1/Questao4.java
@@ -0,0 +1,11 @@
+public class Questao4 {
+        public static void main(String[] args) {
+            Contato pintor1 = new Contato("Donatello", "1234-5678");
+            Contato pintor2 = new Contato("Rafael Sanzio", "2345-6789");
+            Contato pintor3 = new Contato("Sandro Botticelli", "3456-7890");
+    
+            System.out.println(pintor1.toString());
+            System.out.println(pintor2.toString());
+            System.out.println(pintor3.toString());
+        }
+    }
